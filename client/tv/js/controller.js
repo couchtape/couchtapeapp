4aabde4e27f3f46c72979b73d03eaee360f44406
Markus Meineke <mongoviech@gmx.de>
2013-01-19 22:34:53 +0100

    fixed audio player

--- client/tv/js/controller.js
+++ client/tv/js/controller.js
@@ -26,63 +26,22 @@ function playlistCtrl($scope) {
 
     function init() {
         setupWebAudio();
-        setupDrawingCanvas();
-        draw();
     }
 
 
 // Wire up the <audio> element with the Web Audio analyser (currently Webkit only)
     function setupWebAudio() {
-
-        // Get our <audio> element
         audio = document.getElementById('music');
-        // Create a new audio context (that allows us to do all the Web Audio stuff)
-        var audioContext = new webkitAudioContext();
-        // Create a new analyser
-        analyser = audioContext.createAnalyser();
-        // Create a new audio source from the <audio> element
-        var source = audioContext.createMediaElementSource(audio);
-        // Connect up the output from the audio source to the input of the analyser
-        source.connect(analyser);
-        // Connect up the audio output of the analyser to the audioContext destination i.e. the speakers (The analyser takes the output of the <audio> element and swallows it. If we want to hear the sound of the <audio> element then we need to re-route the analyser's output to the speakers)
-        analyser.connect(audioContext.destination);
-
         audio.play();
-
         audio.addEventListener('ended', function () {
-            console.log('ended');
+            next();
         });
-    }
-
-// Draw the audio frequencies to screen
-    function draw() {
-        // Setup the next frame of the drawing
-        webkitRequestAnimationFrame(draw);
-
-        // Create a new array that we can copy the frequency data into
-        var freqByteData = new Uint8Array(analyser.frequencyBinCount);
-        // Copy the frequency data into our new array
-        analyser.getByteFrequencyData(freqByteData);
-
-        // Clear the drawing display
-        canvasContext.clearRect(0, 0, canvas.width, canvas.height);
-
-        // For each "bucket" in the frequency data, draw a line corresponding to its magnitude
-        for (var i = 0; i < freqByteData.length; i++) {
-            canvasContext.fillRect(i, canvas.height - freqByteData[i], 1, canvas.height);
-        }
-    }
-
-// Basic setup for the canvas element, so we can draw something on screen
-    function setupDrawingCanvas() {
-        canvas = document.getElementById('view1');
-        canvasContext = canvas.getContext('2d');
-        canvasContext.fillStyle = '#ffffff';
     }
 
     $scope.songs = [
         {
-            "img": "/api/image/markusmeineke/a62960e5-e0c7-4a98-8344-da6608c802da",
+            //"img": "/api/image/markusmeineke/a62960e5-e0c7-4a98-8344-da6608c802da/small",
+            "img": " ",
             //"file": "http://gp.lc/01 All My Life.mp3",
             "file": "http://gp.lc/04 Dazwischen 2.mp3",
             //"file": "/api/get/markusmeineke/a62960e5-e0c7-4a98-8344-da6608c802da",
@@ -93,57 +52,64 @@ function playlistCtrl($scope) {
             "session": "markusmeineke",
             "_id": "50fade7b3fb1560f16000001"
         }
+,
+        {
+            "img": "/api/image/markusmeineke/a62960e5-e0c7-4a98-8344-da6608c802da",
+            "file": "http://gp.lc/01 All My Life.mp3",
+            //"file": "/api/get/markusmeineke/a62960e5-e0c7-4a98-8344-da6608c802da",
+            "title": "Cherokee",
+            "artist": "Cat Power",
+            "class": "",
+            "id": "a62960e5-e0c7-4a98-8344-da6608c802da",
+            "session": "markusmeineke",
+            "_id": "50fade7b3fb1560f16000001"
+        }
+
     ];
 
     var songId = 0;
 
+    $scope.songs[0].class = 'active';
     $scope.currentSong = $scope.songs[0];
 
     function next() {
-
+        audio.pause();
+        console.dir(audio);
+        console.log('next');
         console.log($scope.songs);
 
         if ($scope.songs.length === 0) {return;}
 
         window.setTimeout(function () {
             $scope.$apply(function () {
-                $scope.songs[0].class = 'active hide';
                 $scope.songs[0].id = 'hide-it';
 
-                $scope.songs[songId].id = '0';
-                $scope.songs[songId].class = 'active';
-                $scope.currentCover = $scope.songs[songId].img;
+                $scope.songs[1].class = 'active';
+                $scope.songs[1].id = '';
+                console.log('song classes');
 
                 window.setTimeout(function () {
-
                     var el = document.getElementById('hide-it');
+                    console.log(el);
                     if (el) {
                         el.style.marginTop = '-' + el.clientHeight + 'px';
                         el.style.opacity = 0;
                         var count = 0;
                         el.addEventListener('webkitTransitionEnd', function () {
+                            console.log('webkitTransitionEnd');
                             if (count === 0) {
                                 $scope.$apply(function () {
-                                    $scope.currentSong = $scope.songs[songId];
                                     $scope.songs.shift();
-
-                                    // Get the <audio> element started
-                                    audio.play();
+                                    $scope.currentSong = $scope.songs[0];
+                                    audio.load();
+                                    window.setTimeout(function () {
+                                        audio.play();
+                                    }, 1);
                                 });
                             }
                             count++;
                         }, false);
-                    } else {
-                        console.log('Loading file: ', $scope.songs[songId].file);
-                        $scope.$apply(function () {
-                            $scope.currentSong = $scope.songs[songId];
-
-                            console.log($scope.currentSong);
-                            // Get the <audio> element started
-                            audio.play();
-                        });
                     }
-                    songId = 1;
                 });
             });
         });
