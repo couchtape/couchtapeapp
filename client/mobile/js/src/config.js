485b0745f77e0f66668a52f9daa168d5f65acafd
Pascal Precht <pascal.precht@googlemail.com>
2013-01-19 21:19:57 +0100

    Fixed view

--- client/mobile/js/src/config.js
+++ client/mobile/js/src/config.js
@@ -3,14 +3,6 @@ angular.module('couchtapeParty', []).config(['$routeProvider', '$locationProvide
     templateUrl: 'mobile/tmpl/current-song.html',
     controller: 'CurrentSongCtrl'
   })
-  .when('/artists', {
-    templateUrl: 'mobile/tmpl/artists.html',
-    controller: 'ArtistsCtrl'
-  })
-  .when('/albums', {
-    templateUrl: 'mobile/tmpl/albums.html',
-    controller: 'AlbumsCtrl'
-  })
   .when('/songs', {
     templateUrl: 'mobile/tmpl/songs.html',
     controller: 'SongsCtrl'
--- client/mobile/js/src/controllers.js
+++ client/mobile/js/src/controllers.js
@@ -1,30 +1,41 @@
 angular.module('couchtapeParty').controller('CurrentSongCtrl', ['$scope', 'CouchtapeService', function ($scope, CouchtapeService) {
+  var promise = CouchtapeService.getPlaylist();
+  var playlist = [];
+
   $scope.song = CouchtapeService.getCurrentSong();
-  $scope.playlist = CouchtapeService.getPlaylist();
-}]);
-
-angular.module('couchtapeParty').controller('ArtistsCtrl', ['$scope', 'CouchtapeService', function ($scope, CouchtapeService) {
-  $scope.artists = CouchtapeService.getArtists();
-}]);
-
-angular.module('couchtapeParty').controller('AlbumsCtrl', ['$scope', 'CouchtapeService', function ($scope, CouchtapeService) {
-
-}]);
-
-angular.module('couchtapeParty').controller('SongsCtrl', ['$scope', 'CouchtapeService', function ($scope, CouchtapeService) {
-  var promise = CouchtapeService.getSongs();
 
   promise.then(function (data) {
-    $scope.songs = data;
-  console.log($scope.songs);
-  }, function (data) {
-     $scope.songs = data; 
+    $scope.playlist = data;
+    console.dir($scope.playlist);
+  }, function (reason) {
+    console.log(reason);
   });
 
 }]);
 
+
+
+angular.module('couchtapeParty').controller('SongsCtrl', ['$scope', 'CouchtapeService', function ($scope, CouchtapeService) {
+
+  var promise = CouchtapeService.getSongs();
+
+  promise.then(function (data) {
+    $scope.songs = data;
+  }, function (data) {
+     $scope.songs = data; 
+  });
+
+  $scope.addSong2Playlist = function (id) {
+    var promise = CouchtapeService.addSong2Playlist(id);
+
+    promise.then(function (data) {
+      alert('Added song to playlist');
+    });
+  };
+}]);
+
 angular.module('couchtapeParty').controller('MenuCtrl', ['$scope', function ($scope) {
-  
+
   $scope.menuOpen = false;
 
   $scope.toggleMenu = function () {
