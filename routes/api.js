acba2b391782050561532cec3321d2f2be7e5c66
Frank Hildebrandt <frank.hildebrandt@sectornord.de>
2013-01-19 14:52:49 +0100

    Work done on the DataStore

--- routes/api.js
+++ routes/api.js
@@ -2,9 +2,23 @@ var api = module.export = exports;
 
 api.db = {};
 
+api.getItemsCollection = function(session,cb) {
+
+    api.db.collection(session, function(err,data){
+
+        if (err) {
+            cb(err);
+        } else {
+            cb(null, data);
+        }
+
+    });
+
+}
+
 api.account = function(){};
 api.tapes = function(){};
 api.file = function(req, res){
-
+    api.db.collection('items')
 };
 api.artists = function(){};
