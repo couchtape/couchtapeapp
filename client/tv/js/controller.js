86dcb7b12d582090c92ba327e923a9bedd704e00
Markus Meineke <mongoviech@gmx.de>
2013-01-19 23:28:59 +0100

    fixed enqueue

--- client/tv/js/controller.js
+++ client/tv/js/controller.js
@@ -265,15 +265,17 @@ function playlistCtrl($scope, $document, CouchtapeService) {
 
     document.onEnqueue = function (data) {
         console.log('data', data);
-        var start = false;
-        if ($scope.songs.length === 0) {
-            start = true;
-        }
-        $scope.songs.push(data);
-        if (start) {
-            $scope.songs[0].class = 'active';
-            $scope.currentSong = $scope.songs[0];
-        }
+        $scope.$apply(function () {
+            var start = false;
+            if ($scope.songs.length === 0) {
+                start = true;
+            }
+            $scope.songs.push(data);
+            if (start) {
+                $scope.songs[0].class = 'active';
+                $scope.currentSong = $scope.songs[0];
+            }
+        });
     };
 
     $scope.next = function () {
